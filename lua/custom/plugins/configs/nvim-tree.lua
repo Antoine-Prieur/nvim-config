630efd51fc6c0085adac83077065b275fa3794e3
antoine-prieur1 <antoine.prieur@celliersintelligents.com>
2024-01-17 20:37:37 -0500

    style: improve diagnostic disply

--- lua/custom/plugins/configs/nvim-tree.lua
+++ lua/custom/plugins/configs/nvim-tree.lua
@@ -1,71 +1,72 @@
-require("nvim-tree").setup(
-  {
-    view = {
-      adaptive_size = false,
-      side = "left",
-      width = 30,
-      preserve_window_proportions = true,
+require('nvim-tree').setup {
+  view = {
+    adaptive_size = false,
+    side = 'left',
+    width = 30,
+    preserve_window_proportions = true,
+  },
+  git = {
+    enable = false,
+    ignore = true,
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  modified = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = false,
+  },
+  filesystem_watchers = {
+    enable = true,
+  },
+  actions = {
+    open_file = {
+      resize_window = true,
     },
-    git = {
+  },
+  renderer = {
+    root_folder_label = false,
+    highlight_git = false,
+    highlight_diagnostics = true,
+    highlight_opened_files = 'none',
+
+    indent_markers = {
       enable = false,
-      ignore = true,
     },
-    diagnostics = {
-      enable = true,
-      show_on_dirs = true,
-    },
-    modified = {
-      enable = true,
-      show_on_dirs = true,
-    },
-    filesystem_watchers = {
-      enable = true,
-    },
-    actions = {
-      open_file = {
-        resize_window = true,
-      },
-    },
-    renderer = {
-      root_folder_label = false,
-      highlight_git = false,
-      highlight_opened_files = "none",
 
-      indent_markers = {
-        enable = false,
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+        diagnostics = false,
       },
-
-      icons = {
-        show = {
-          file = true,
-          folder = true,
-          folder_arrow = true,
-          git = true,
+      glyphs = {
+        default = '󰈚',
+        symlink = '',
+        folder = {
+          default = '',
+          empty = '',
+          empty_open = '',
+          open = '',
+          symlink = '',
+          symlink_open = '',
+          arrow_open = '',
+          arrow_closed = '',
         },
-        glyphs = {
-          default = "󰈚",
-          symlink = "",
-          folder = {
-            default = "",
-            empty = "",
-            empty_open = "",
-            open = "",
-            symlink = "",
-            symlink_open = "",
-            arrow_open = "",
-            arrow_closed = "",
-          },
-          git = {
-            unstaged = "✗",
-            staged = "✓",
-            unmerged = "",
-            renamed = "➜",
-            untracked = "★",
-            deleted = "",
-            ignored = "◌",
-          },
+        git = {
+          unstaged = '✗',
+          staged = '✓',
+          unmerged = '',
+          renamed = '➜',
+          untracked = '★',
+          deleted = '',
+          ignored = '◌',
         },
       },
     },
-  }
-)
+  },
+}
