cf8cc40aa06dc00c66dc40441bc320cadb8c261e
antoine-prieur1 <antoine.prieur@celliersintelligents.com>
2024-01-17 20:12:30 -0500

    feat: execute mypy with venv

--- lua/custom/plugins/configs/lspconfig.lua
+++ lua/custom/plugins/configs/lspconfig.lua
@@ -1,12 +1,12 @@
-local config = require("lspconfig.configs")
+local config = require 'lspconfig.configs'
 
 local on_attach = config.on_attach
 local capabilities = config.capabilities
 local tf_capb = vim.lsp.protocol.make_client_capabilities()
 tf_capb.textDocument.completion.completionItem.snippetSupport = true
 
-local lspconfig = require("lspconfig")
-local util = require('lspconfig/util')
+local lspconfig = require 'lspconfig'
+local util = require 'lspconfig/util'
 
 local path = util.path
 
@@ -16,7 +16,7 @@ local function get_python_path(workspace)
   -- Find and use virtualenv via poetry in workspace directory.
   local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
   if match ~= '' then
-    local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
+    local venv = vim.fn.trim(vim.fn.system 'poetry env info -p')
     return path.join(venv, 'bin', 'python')
   end
 
@@ -26,7 +26,7 @@ local function get_python_path(workspace)
   end
 
   -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({'*', '.*'}) do
+  for _, pattern in ipairs { '*', '.*' } do
     local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
     if match ~= '' then
       return path.join(path.dirname(match), 'bin', 'python')
@@ -34,26 +34,43 @@ local function get_python_path(workspace)
   end
 
   -- Fallback to system Python.
-  return exepath('python3') or exepath('python') or 'python'
+  return exepath 'python3' or exepath 'python' or 'python'
 end
 
 -- Setup --
 
-lspconfig.pyright.setup({
+lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"python"},
+  filetypes = { 'python' },
   before_init = function(_, config_arg)
     config_arg.settings.python.pythonPath = get_python_path(config.root_dir)
   end,
   settings = {
-  python = {
-    analysis = {
-      autoSearchPaths = true,
-      diagnosticMode = "workspace",
-      useLibraryCodeForTypes = true
-      }
-    }
-  }
-})
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = 'workspace',
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+}
 
+-- Mypy --
+
+local mypy = require('lint').linters.mypy
+
+mypy.args = {
+  args = {
+    '--show-column-numbers',
+    '--show-error-end',
+    '--hide-error-codes',
+    '--hide-error-context',
+    '--no-color-output',
+    '--no-error-summary',
+    '--no-pretty',
+    '--python-executable',
+    get_python_path(config.root_dir),
+  },
+}
